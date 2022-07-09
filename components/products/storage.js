891116ec2fe75363b359a1a2f9e9715ffb58565a
abarroso <abarroso@saintnet.com>
2022-07-08 20:32:35 -0400

    [FEATURE] Changed Storage of products,forgotcommit

--- components/products/storage.js
+++ components/products/storage.js
@@ -12,6 +12,12 @@ function getProductById(productId) {
   return ProductModel.find({ productId });
 }
 
+function modifyProduct(product) {
+  return new ProductModel.find({ _id: product.productId }).modifyOne({
+    ...product
+  });
+}
+
 function deleteProduct(productId) {
   return ProductModel.find({ _id: productId }).deleteOne({
     _id: productId,
@@ -22,5 +28,6 @@ module.exports = {
   createProduct,
   getAllProducts,
   getProductById,
+  modifyProduct,
   deleteProduct,
 };
