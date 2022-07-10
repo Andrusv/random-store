b6f3589cc9357d88ac08e53558b327c8b4d0e638
abarroso <abarroso@saintnet.com>
2022-07-09 21:05:25 -0400

    [FIX] Changed some http status code in network

--- components/products/network.js
+++ components/products/network.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.post("/", (req, res) => {
   createProduct(req.body.products)
-    .then((products) => response.success(req, res, products, 200))
+    .then((products) => response.success(req, res, products, 201))
     .catch((error) =>
       response.error(req, res, "Error en base de datos", null, error)
     );
@@ -19,7 +19,7 @@ router.get("/", (req, res) => {
   getAllProducts(req.body.productId, req.query.limit)
     .then((products) => response.success(req, res, products, 200))
     .catch((error) =>
-      response.error(req, res, "Error en base de datos", null, error)
+      response.error(req, res, "Error en base de datos", 404, error)
     );
 });
 
@@ -27,7 +27,7 @@ router.patch("/", (req, res) => {
   modifyProduct(req.body.Product)
     .then((productUpdated) => response.success(req, res, productUpdated, 200))
     .catch((error) =>
-      response.error(req, res, "Error en base de datos", null, error)
+      response.error(req, res, "Error en base de datos", 304, error)
     );
 });
 
@@ -35,7 +35,7 @@ router.delete("/", (req, res) => {
   deleteProduct(req.body.productId)
     .then((deletedProduct) => response.success(req, res, deletedProduct, 200))
     .catch((error) =>
-      response.error(req,res,"Error en base de datos",null,error
+      response.error(req,res,"Error en base de datos",409,error
       )
     );
 });
