2c6ac7be85171e5e16660700d9618f74360cb6a2
abarroso <abarroso@saintnet.com>
2022-07-20 18:40:54 -0400

    [FEATURE] Included user data in getCustomerById()

--- components/customers/storage.js
+++ components/customers/storage.js
@@ -1,19 +1,24 @@
 const { CustomersModel } = require('./model');
+const { UsersModel } = require('../users/model');
 
 function createCustomer(customer) {
-  return new CustomersModel(customer).save()
+  return new CustomersModel(customer).save();
 }
 
 function getAllCustomers(limit) {
-  return CustomersModel.findAll({limit});
+  return CustomersModel.findAll({ limit });
 }
 
 function getCustomerById(customerId) {
-  return CustomersModel.findByPk(customerId);
+  return CustomersModel.findByPk(customerId, {
+    include: [{
+      model: UsersModel
+    }],
+  });
 }
 
 function modifyCustomer(customer, customerId) {
-  return CustomersModel.update(customer, { where: { id: customerId } })
+  return CustomersModel.update(customer, { where: { id: customerId } });
 }
 
 function deleteCustomer(customerId) {
